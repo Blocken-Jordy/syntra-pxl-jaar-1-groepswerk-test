f71c32994b3ef5fe5cfe905aecc824dec1823d23
Ben Geuten <ben.gm@outlook.be>
2023-04-25 20:56:16 +0200

    Tripel/upgrade knop aangemaakt en een hidden counter toegevoegd.

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 let counter = 0;
+let hiddenCounter = 0;
 
 const KlikbareJupilerKnop = document.querySelector(".jupiler-button");
+const tripel = document.querySelector('.tripel-button');
+tripel.disabled = true;
 
-
-KlikbareJupilerKnop.addEventListener('click', () => {
+KlikbareJupilerKnop.onclick = function () {
     counter++;
+    hiddenCounter++;
     console.log(counter);
-});
+
+    if (hiddenCounter === 10) {
+        KlikbareJupilerKnop.disabled = true;
+        tripel.disabled = false;
+    }
+    };
+    tripel.onclick = function () {
+        counter += 2;
+        KlikbareJupilerKnop.disabled = false;
+        tripel.disabled = true;
+        console.log(counter);
+        hiddenCounter =0;
+    };
+
+
+
 
