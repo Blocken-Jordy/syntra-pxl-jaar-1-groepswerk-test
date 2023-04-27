32546242430e7c0ecbe2b1ae7d237e940ba7617d
BenG <ben.gm@outlook.be>
2023-04-27 21:47:16 +0200

    upgrade/sixpack knop aangemaakt voor iedere 10s, 1 op te tellen

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 let counter = 0;
 let hiddenCounter = 0;
+let sixpackCounter = 0;
+let sixpackInterval;
 
 const klikbareJupilerKnop = document.querySelector(".jupiler-button");
 const tripel = document.querySelector(".tripel-button");
 const restartButton = document.querySelector(".restart-button");
 let num = document.getElementById("coun");
-const upgrade = document.getElementById("up");
+const sixpack = document.getElementById("sixpack");
 
 tripel.disabled = true;
 
+
+//Jupiler knop +1
 klikbareJupilerKnop.onclick = function () {
   counter++;
   hiddenCounter++;
@@ -19,11 +23,13 @@ klikbareJupilerKnop.onclick = function () {
     klikbareJupilerKnop.disabled = true;
     tripel.disabled = false;
   }
-  if (counter === 20) {
-    upgrade.disabled = false;
-    alert("gooed");
+
+  if (counter === 10) {
+    sixpack.disabled = false;
   }
 };
+
+// Tripel knop +2
 tripel.onclick = function () {
   counter += 2;
   klikbareJupilerKnop.disabled = false;
@@ -34,7 +40,36 @@ tripel.onclick = function () {
   hiddenCounter = 0;
 };
 
+//restart knop
 restartButton.addEventListener("click", () => {
   counter = 0;
   console.log(counter);
+  sixpackCounter = 0;
+    clearInterval(sixpackInterval);
 });
+
+//sixpack knop - iedere 10s telt hij +1
+sixpack.onclick = function () {
+    counter -= 10;
+    sixpackCounter += 1;
+    num.value = counter;
+    timer();
+}
+
+function timer () {
+    if (sixpackInterval) {
+        clearInterval(sixpackInterval)
+    }
+    sixpackInterval = setInterval(() => {
+        counter += sixpackCounter;
+        num.value = counter;
+        console.log(counter);
+        if (counter < 10) {
+            sixpack.disabled = true;
+        } else {
+            sixpack.disabled = false;
+        }
+    }, 1000);
+    
+}
+
