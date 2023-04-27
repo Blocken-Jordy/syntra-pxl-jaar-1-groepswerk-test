e788fd3533ad2e92593d7eaeb75778145fc14d95
Diego Chicas <diegojosue62@gmail.com>
2023-04-27 19:32:18 +0200

    couenter op de input

--- index.js
+++ index.js
@@ -2,35 +2,34 @@ let counter = 0;
 let hiddenCounter = 0;
 
 const klikbareJupilerKnop = document.querySelector(".jupiler-button");
-const tripel = document.querySelector('.tripel-button');
-const restartButton = document.querySelector(".restart-button")
+const tripel = document.querySelector(".tripel-button");
+const restartButton = document.querySelector(".restart-button");
+let num = document.getElementById("coun");
 
 tripel.disabled = true;
 
 klikbareJupilerKnop.onclick = function () {
-    counter++;
-    hiddenCounter++;
-    console.log(counter);
+  counter++;
+  hiddenCounter++;
+  num.value = counter;
+  console.log(counter);
 
-    if (hiddenCounter === 10) {
-        klikbareJupilerKnop.disabled = true;
-        tripel.disabled = false;
-    }
-    };
-    tripel.onclick = function () {
-        counter += 2;
-        klikbareJupilerKnop.disabled = false;
-        tripel.disabled = true;
-        console.log(counter);
-        hiddenCounter =0;
-    };
+  if (hiddenCounter === 10) {
+    klikbareJupilerKnop.disabled = true;
+    tripel.disabled = false;
+  }
+};
+tripel.onclick = function () {
+  counter += 2;
+  klikbareJupilerKnop.disabled = false;
+  tripel.disabled = true;
+  console.log(counter);
+  num.value = counter;
 
+  hiddenCounter = 0;
+};
 
-
-
-
-restartButton.addEventListener('click', () => {
-    counter = 0;
-    console.log(counter);
+restartButton.addEventListener("click", () => {
+  counter = 0;
+  console.log(counter);
 });
-
